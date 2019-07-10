16e849912ed9797d8919bdedb0143a631b76ab7a
Dylan <initialflux@gmail.com>
2019-07-10 03:42:39 -0500

    implemented sign-up feature with email and password

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends React.Component {
               ...snapshot.data()
             }
           });
+
+          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
